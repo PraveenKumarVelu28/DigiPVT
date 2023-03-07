282d5e5019d33a9388c6e7002028a91c8f903123
Praveen <Praveenkumaramze@gmail.com>
2023-03-07 12:53:35 +0530

    ss

--- src/assets/Images/revamp/src/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: false,
-  basehost:'https://asticom.digiofficeapp.com/AsticomMainAPI',
-  apiurl:'/MobileUser/GetCompanyID?CompanyID=',
-  host1:'https://asticom.digiofficeapp.com/SupportAPI' 
-};
